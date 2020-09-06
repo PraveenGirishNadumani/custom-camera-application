564d5b6278e47f5696d7f27f6cfd63c149f998fd
PraveenGirishNadumani <praveen.gn@razorpay.com>
2020-09-06 22:43:52 +0530

    Adding cloud support - Firebase with loading UI

--- app/src/main/java/com/example/customcameraapp/MainActivity.java
+++ app/src/main/java/com/example/customcameraapp/MainActivity.java
@@ -12,11 +12,14 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Matrix;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.util.Rational;
 import android.util.Size;
 import android.view.Surface;
@@ -25,7 +28,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
 import java.io.File;
+import java.net.URI;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -33,6 +48,11 @@ public class MainActivity extends AppCompatActivity {
     private String[] REQUEST_PERMISSIONS = new String[]{"android.permission.CAMERA","android.permission.WRITE_EXTERNAL_STORAGE"};
 
     TextureView textureView;
+    String imageNamge = "Image";
+    ProgressDialog progressDialog;
+
+    private FirebaseStorage storage;
+    private StorageReference storageReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +60,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         getSupportActionBar().hide();
+
+        storage = FirebaseStorage.getInstance();
+        storageReference = storage.getReference();
 
         textureView = (TextureView) findViewById(R.id.view_finder);
 
@@ -87,12 +110,31 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 
-                File file = new File(Environment.getDataDirectory()+"/data/com.example.customcameraapp/cameraX");
+//                takePhoto();
+                imageNamge += System.currentTimeMillis() + ".jpg";
+                File file = new File(Environment.getDataDirectory()+"/data/com.example.customcameraapp/"+imageNamge);
+//                File xfile = new File(Environment.getDataDirectory()+"/data/"+System.currentTimeMillis());
                 imgCap.takePicture(file, new ImageCapture.OnImageSavedListener() {
                     @Override
                     public void onImageSaved(@NonNull File file) {
                         String msg = "image Captured at" + file.getAbsolutePath();
+
                         Toast.makeText(getBaseContext(), msg,Toast.LENGTH_LONG).show();
+
+                        File folder = new File(Environment.getDataDirectory()+"/data/com.example.customcameraapp/"+imageNamge);
+                        uploadImage(Uri.fromFile(folder));
+
+//                        if(listOfFolders!=null) {
+//                            for (File image : listOfFolders) {
+//
+//                                if (image.isFile()) {
+//                                    File x = new File(Environment.getDataDirectory()+"/data/com.example.customcameraapp/1599405645264.jpg");
+//                                    uploadImage(Uri.fromFile(x));
+//                                }
+//                            }
+//                        }else {
+//                            Toast.makeText(getBaseContext(),"no files to upload",Toast.LENGTH_SHORT).show();
+//                        }
                     }
 
                     @Override
@@ -111,6 +153,47 @@ public class MainActivity extends AppCompatActivity {
 
         CameraX.bindToLifecycle(this,preview,imgCap );
 
+    }
+
+//    private void takePhoto() {
+//
+//        File photoFile = new File()
+//    }
+
+    private void uploadImage(android.net.Uri file) {
+
+
+        startLoadingUI();
+        StorageReference riversRef = storageReference.child("images/"+imageNamge);
+
+        riversRef.putFile(file)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        progressDialog.dismiss();
+                        Snackbar.make(findViewById(R.id.view_finder),"Image Uploaded", Snackbar.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        progressDialog.dismiss(); 
+                        Toast.makeText(getBaseContext(),"Failed",Toast.LENGTH_SHORT).show();
+                        // ...
+                    }
+                });
+    }
+
+    private void startLoadingUI() {
+
+        progressDialog = new ProgressDialog(MainActivity.this);
+        progressDialog.show();
+        progressDialog.setContentView(R.layout.progress_dialog);
+        progressDialog.getWindow().setBackgroundDrawableResource(
+                android.R.color.transparent
+        );
     }
 
     private void updateTransform() {
